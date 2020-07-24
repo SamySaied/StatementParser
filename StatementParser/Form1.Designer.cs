dbc475518df067f73b4a580b3d1c33427ae2e09c
Samy Saied <samy.saied@gmail.com>
2020-07-24 12:42:27 -0400

    First Commit

---
+++ StatementParser/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace StatementParser
+{
+    partial class FormParser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxOriginal = new System.Windows.Forms.TextBox();
+            this.textBoxParsed = new System.Windows.Forms.TextBox();
+            this.buttonParse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxOriginal
+            // 
+            this.textBoxOriginal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBoxOriginal.Location = new System.Drawing.Point(13, 13);
+            this.textBoxOriginal.Multiline = true;
+            this.textBoxOriginal.Name = "textBoxOriginal";
+            this.textBoxOriginal.Size = new System.Drawing.Size(600, 572);
+            this.textBoxOriginal.TabIndex = 0;
+            // 
+            // textBoxParsed
+            // 
+            this.textBoxParsed.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxParsed.Location = new System.Drawing.Point(749, 13);
+            this.textBoxParsed.Multiline = true;
+            this.textBoxParsed.Name = "textBoxParsed";
+            this.textBoxParsed.Size = new System.Drawing.Size(600, 572);
+            this.textBoxParsed.TabIndex = 1;
+            // 
+            // buttonParse
+            // 
+            this.buttonParse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonParse.Location = new System.Drawing.Point(620, 13);
+            this.buttonParse.Name = "buttonParse";
+            this.buttonParse.Size = new System.Drawing.Size(121, 572);
+            this.buttonParse.TabIndex = 2;
+            this.buttonParse.Text = "&Parse";
+            this.buttonParse.UseVisualStyleBackColor = true;
+            // 
+            // FormParser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1361, 597);
+            this.Controls.Add(this.buttonParse);
+            this.Controls.Add(this.textBoxParsed);
+            this.Controls.Add(this.textBoxOriginal);
+            this.Name = "FormParser";
+            this.Text = "Statement Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxOriginal;
+        private System.Windows.Forms.TextBox textBoxParsed;
+        private System.Windows.Forms.Button buttonParse;
+    }
+}
+
